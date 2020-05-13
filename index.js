a4316c1c1a08425b474c94e477a61160adb4e193
Joshua R Pukin <joshuapukin@gmail.com>
2020-05-13 02:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var emptArray = []
+  for(var i=0; i < musicians.length; i++){
+      const sen = musicians[i] + ' plays ' + instruments[i]
+      emptArray.push(sen);
+  }
+  return emptArray;
+}
+function johnLennonFacts(facts){
+  let i = 0;
+  while(i<facts.length) {
+    facts[i] += '!!!'
+    i++;
+  }
+ console.log(facts)
+}
+johnLennonFacts(facts);
